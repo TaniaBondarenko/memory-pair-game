c0e63b20361cfce24391eefd0a853bdb037bea7d
Tetiana Bondarenko <tn.a.bondarenko@gmail.com>
2020-12-22 22:43:59 +0200

    fixed doble click

--- app.js
+++ app.js
@@ -67,15 +67,14 @@
     ];
 
 window.addEventListener("load", init);
-    const main_image = "images/bg_card.png";
+
     const game_board = document.querySelector(".game_board");
-    const resultDisplay = document.querySelector(".result");
-    let attempts = 0;
     let cardsChosen = [];
     let cardsChosenId = [];
     let cardsGuessed = [];
-
-    function shuffle() {
+    let attempts = 0;
+   
+function shuffle() {
         for (i = card_obj.length - 1; i > 0; i--) {
             j = Math.floor(Math.random() * i);
             k = card_obj[i];
@@ -88,60 +87,64 @@ function createBoard() {
     for (let i = 0; i < card_obj.length; i++) {
         let cardFront = document.createElement("img");
         cardFront.className = "card_front";
-        cardFront.setAttribute("alt", "x-mas icon");
         cardFront.setAttribute("data-id", i);
-        cardFront.src = main_image;
+        cardFront.src = "images/front_card.png";
         game_board.appendChild(cardFront);
         cardFront.addEventListener("click", flipCard);
     }
 }
 
 function flipCard() {
-    console.log("flipCard");
     let cardId = this.getAttribute("data-id");
-        cardsChosen.push(card_obj[cardId].name);
-        cardsChosenId.push(cardId);
+    cardsChosen.push(card_obj[cardId].name);
+    cardsChosenId.push(cardId);
     this.setAttribute("src", card_obj[cardId].img);
     this.classList.add(".clicked");
-        //console.log("this", this);
     if (cardsChosen.length === 2) {
         setTimeout(checkForMatch, 500);
         }
         
     function checkForMatch() {
-            console.log("check4match");
-            let cards = document.querySelectorAll("img");
+            let cardsAll = document.querySelectorAll("img");
             const firstCardId = cardsChosenId[0];
             const secondCardId = cardsChosenId[1];
-        if (cardsChosen[0] === cardsChosen[1]) {
-            cards[firstCardId].setAttribute("src", "images/bg_card2.png");
-            cards[firstCardId].setAttribute("class", "glass");
-            cards[secondCardId].setAttribute("src", "images/bg_card2.png");
-            cards[secondCardId].setAttribute("class", "glass");
+        if ((cardsChosen[0] === cardsChosen[1])&&(firstCardId !== secondCardId) ){
+            cardsAll[firstCardId].setAttribute("src", "images/just-card.png");
+            cardsAll[firstCardId].setAttribute("class", "glass");
+            cardsAll[secondCardId].setAttribute("src", "images/just-card.png");
+            cardsAll[secondCardId].setAttribute("class", "glass");
             cardsGuessed.push(cardsChosen);
             attempts += 1;
-        } else {
-            cards[firstCardId].setAttribute("src", main_image);
-            cards[secondCardId].setAttribute("src", main_image);
+        } else  {
+            cardsAll[firstCardId].setAttribute("src", "images/front_card.png");
+            cardsAll[secondCardId].setAttribute("src", "images/front_card.png");
             attempts += 1;
         }
+
         cardsChosen = [];
         cardsChosenId = [];
-    let allclicked = document.getElementsByClassName(".clicked");
-    Array.from(allclicked).forEach((item) => item.classList.remove(".clicked"));
-        resultDisplay.textContent = cardsGuessed.length;
+        removeClick();
+        showGameResult();
+    }
+    
+    function removeClick() {
+        let allClicked = document.getElementsByClassName(".clicked");
+        Array.from(allClicked).forEach((item) => item.classList.remove(".clicked"));
+    }
+    
+    function showGameResult() {
+        const resultDisplay = document.querySelector(".result");
+        resultDisplay.textContent = `Pairs guessed: ${cardsGuessed.length}`;
         if (cardsGuessed.length === card_obj.length / 2) {
             resultDisplay.textContent = `Good job! Your number of attempts ${attempts}`;
-        
             }
-        }
-        
+    }
+
         
 };
 
 function init() {
     createBoard();
     shuffle();
-  
 }
 
