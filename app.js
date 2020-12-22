d6eb2e705031d86961619d905b28f5d5cb736568
Tetiana Bondarenko <tn.a.bondarenko@gmail.com>
2020-12-22 16:22:36 +0200

    version3

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-    const card_path = [
+    const card_obj = [
         {
             name: "bear",
             img: "images/bear.png"
@@ -67,68 +67,81 @@
     ];
 
 window.addEventListener("load", init);
-    const front_img = "images/bg_card.png";
-    const grid = document.querySelector(".grid");
+    const main_image = "images/bg_card.png";
+    const game_board = document.querySelector(".game_board");
     const resultDisplay = document.querySelector(".result");
+    let attempts = 0;
     let cardsChosen = [];
     let cardsChosenId = [];
-    let cardsWon = [];
+    let cardsGuessed = [];
 
     function shuffle() {
-        for (i = card_path.length - 1; i > 0; i--) {
+        for (i = card_obj.length - 1; i > 0; i--) {
             j = Math.floor(Math.random() * i);
-            k = card_path[i];
-            card_path[i] = card_path[j];
-            card_path[j] = k;
+            k = card_obj[i];
+            card_obj[i] = card_obj[j];
+            card_obj[j] = k;
         }
     }
         
-    function createBoard() {
-            for (let i = 0; i < card_path.length; i++) {
-                let cardFront = document.createElement("img");
-                cardFront.className = "front_img ";
-                //cardFront.setAttribute("alt", "x-mas icon");
-                cardFront.setAttribute("data-id", i);
-                cardFront.src = front_img;
-                cardFront.addEventListener("click", flipCard)
-                grid.appendChild(cardFront);
-        }
-    
+function createBoard() {
+    for (let i = 0; i < card_obj.length; i++) {
+        let cardFront = document.createElement("img");
+        cardFront.className = "card_front";
+        cardFront.setAttribute("alt", "x-mas icon");
+        cardFront.setAttribute("data-id", i);
+        cardFront.src = main_image;
+        game_board.appendChild(cardFront);
+        cardFront.addEventListener("click", flipCard);
     }
+}
 
-    function flipCard() {
-            let cardId = this.getAttribute("data-id");
-            cardsChosen.push(card_path[cardId].name);
-            cardsChosenId.push(cardId);
-            this.setAttribute("src", card_path[cardId].img);
-            if (cardsChosen.length === 2) {
-                setTimeout(checkForMatch, 500);
-            }
-        };
-
+function flipCard() {
+    console.log("flipCard");
+    let cardId = this.getAttribute("data-id");
+        cardsChosen.push(card_obj[cardId].name);
+        cardsChosenId.push(cardId);
+    this.setAttribute("src", card_obj[cardId].img);
+    this.classList.add(".clicked");
+        //console.log("this", this);
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500);
+        }
+        
     function checkForMatch() {
+            console.log("check4match");
             let cards = document.querySelectorAll("img");
-            const optionOneId = cardsChosenId[0];
-            const optionTwoId = cardsChosenId[1];
+            const firstCardId = cardsChosenId[0];
+            const secondCardId = cardsChosenId[1];
         if (cardsChosen[0] === cardsChosen[1]) {
-                cards[optionOneId].setAttribute("src", "images/santa.png");
-                cards[optionTwoId].setAttribute("src", "images/santa.png");
-                cardsWon.push(cardsChosen);
+            cards[firstCardId].setAttribute("src", "images/bg_card2.png");
+            cards[firstCardId].setAttribute("class", "glass");
+            cards[secondCardId].setAttribute("src", "images/bg_card2.png");
+            cards[secondCardId].setAttribute("class", "glass");
+            cardsGuessed.push(cardsChosen);
+            attempts += 1;
         } else {
-                cards[optionOneId].setAttribute("src", front_img);
-                cards[optionTwoId].setAttribute("src", front_img);
+            cards[firstCardId].setAttribute("src", main_image);
+            cards[secondCardId].setAttribute("src", main_image);
+            attempts += 1;
         }
-         cardsChosen = [];
-         cardsChosenId = [];
-            resultDisplay.textContent = cardsWon.length;
-            if (cardsWon.length === card_path.length / 2) {
-            resultDisplay.textContent = "Good job!";
-         }
+        cardsChosen = [];
+        cardsChosenId = [];
+    let allclicked = document.getElementsByClassName(".clicked");
+    Array.from(allclicked).forEach((item) => item.classList.remove(".clicked"));
+        resultDisplay.textContent = cardsGuessed.length;
+        if (cardsGuessed.length === card_obj.length / 2) {
+            resultDisplay.textContent = `Good job! Your number of attempts ${attempts}`;
+        
+            }
         }
+        
+        
+};
 
 function init() {
     createBoard();
     shuffle();
+  
 }
-
 
